02f6c4ed921c4ee3384a44687c428c2be9a0bf6f
leynier <leynier41@gmail.com>
2018-07-20 10:20:52 -0400

    Modificar la estructura de config.py del módulo core
    La estructura de las transiciones paso de ser una tupla
    de dos elementos a ser un diccionarios con las llaves
    'query' y 'next_module'.
    
    Se implementaron las clases Module y Transition y el
    método get_config que devuelve un diccionario mas cómodo
    de utilizar.
    
    Ejemplos:
    
    Para acceder a la url del módulo:
    	Antes: config['modulo']['url']
    	Ahora: config['modulo'].url
    
    Al igual pasa con las transiciones:
    	Antes: config['module']['transitions'][0]['query']
    	Ahora: config['module'].transitions[0].query
    
    Se creo el archivo test_core.py y la clase ConfigTestCase
    para realizar los tests

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,75 @@
-config = {
+_config = {
     'etiqueta_modulo': {
         'url': 'http://www.example.com',
         'tags': ['Tag1', 'Tag2'],
         'transitions': [
-            (lambda tags_list: True, 'proxima_etiqueta_modulo1'),
-            (lambda tags_list: False, 'proxima_etiqueta_modulo2')
+            {
+                'query': lambda tags_list: True,
+                'next_module': 'proxima_etiqueta_modulo1'
+            },
+            {
+                'query': lambda tags_list: False,
+                'next_module': 'proxima_etiqueta_modulo2'
+            }
         ]
     }
 }
+
+
+class Transition(object):
+    """Clase para facilitar la utilizacion de un diccionario de la forma:
+    {
+        'query': lambda tags_list: False,
+        'next_module': 'proxima_etiqueta_modulo2'
+    }
+
+    Raises:
+        Exception -- Lanza una excepción si algún argumento es nulo
+    """
+    def __init__(self, query, next_module):
+        if not query or not next_module:
+            raise Exception('Ningún argumento puede ser nulo.')
+        self.query = query
+        self.next_module = next_module
+
+
+class Module(object):
+    """Clase para facilitar la utilizacion de un diccionario de la forma:
+    {
+        'url': 'http://www.example.com',
+        'tags': ['Tag1', 'Tag2'],
+        'transitions': [...]
+    }
+
+    Raises:
+        Exception -- Lanza una excepción si algún argumento es nulo
+    """
+    def __init__(self, url, tags, transitions):
+        if not url or not tags or not transitions:
+            raise Exception('Ningún argumento puede ser nulo.')
+        self.url = url
+        self.tags = tags
+        self.transitions = transitions
+
+
+def get_config():
+    """Devolver un diccionario en el que los varlos son
+    de tipo Module
+
+    Returns:
+        dict -- Diccionario de la forma {key: instance of Module}
+    """
+    new_config = {}
+    for key in _config:
+        _module = _config[key]
+        url = _module['url']
+        tags = _module['tags']
+        transitions = []
+        for _transitions in _module['transitions']:
+            _query = _transitions['query']
+            _next_module = _transitions['next_module']
+            transition = Transition(_query, _next_module)
+            transitions.append(transition)
+        module = Module(url, tags, transitions)
+        new_config[key] = module
+    return new_config
---
+++ tests/test_core.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from app.core.config import _config, get_config
+
+class ConfigTestCase(TestCase):
+    def setUp(self):
+        self.config = get_config()
+
+    def test_get_config(self):
+        # Comprobar si las llaves son iguales
+        for key1, key2 in zip(_config, self.config):
+            self.assertEqual(key1, key2)
+        for key in _config:
+            module1 = _config[key]
+            module2 = self.config[key]
+            # Comprobar si las urls son iguales
+            self.assertEqual(module1['url'], module2.url)
+            # Comprobar si los etiquetas son iguales
+            for tag1, tag2 in zip(module1['tags'], module2.tags):
+                self.assertEqual(tag1, tag2)
+            # Comprobar si las transiciones son iguales
+            for trans1, trans2 in zip(module1['transitions'], module2.transitions):
+                self.assertEqual(trans1['query'], trans2.query)
+                self.assertEqual(trans1['next_module'], trans2.next_module)
