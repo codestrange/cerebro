8f341dd0f197c74daad339b7fb91beb0f9555a8c
leynier <leynier41@gmail.com>
2018-07-20 11:47:31 -0400

    Añadir el documento Message de Mongo al shell de Flask

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from os import getenv
 from app import create_app
-from app.models import db
+from app.models import db, Message
 from app.core import start_core
 
 app = create_app(getenv('FLASK_CONFIG') or 'default')
@@ -15,4 +15,4 @@ def test():
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db)
+    return dict(db=db, Message=Message)
