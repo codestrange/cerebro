dd31a239ee7fea683b10736ae59bae7a448bddc4
leynier <leynier41@gmail.com>
2018-07-20 14:29:31 -0400

    Iniciar el método start_core en un hilo independiente
    Por ahora creó que soluciona el problemas pero me parece
    que sería mejor utilizar la clase asyncio y corutinas.

--- run.py
+++ run.py
@@ -4,7 +4,18 @@ from app.models import db, Message
 from app.core import start_core
 
 app = create_app(getenv('FLASK_CONFIG') or 'default')
-start_core()
+
+# El módulo dummy_threading.threading provee una interfaz igual a la del módulo
+# threading porque threading utilizá el módulo _thread y este no es provisto por
+# todas las plataformas.
+try:
+    from threading import Thread
+except ImportError:
+    from dummy_threading.threading import Thread
+
+# Corriendo el módulo core en un hilo independiente del de flask
+core_thread = Thread(target=start_core)
+core_thread.start()
 
 @app.cli.command()
 def test():
