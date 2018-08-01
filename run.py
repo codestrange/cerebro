69d933061f982f0958c85a2d7405be03bc3bb328
leynier <leynier41@gmail.com>
2018-07-31 22:48:02 -0400

    Añadir Code Coverage a la aplicación

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from os import getenv
+from os.path import abspath, dirname, join
+from coverage import coverage
 from flask_script import Manager, Shell
+
+# Coverage debe empezar antes de las importaciones
+# de los módulos de las aplicación
+COV = coverage(branch=True, include='app/*')
+COV.start()
+
 from app import create_app
 from app.models import db, Message
 from app.core import start_core
@@ -25,6 +33,15 @@ def test():
     from unittest import TestLoader, TextTestRunner
     tests = TestLoader().discover('tests')
     TextTestRunner(verbosity=2).run(tests)
+    COV.stop()
+    COV.save()
+    print('\nCoverage Summary:')
+    COV.report()
+    basedir = abspath(dirname(__file__))
+    covdir = join(basedir, 'tmp/coverage')
+    COV.html_report(directory=covdir)
+    print(f'HTML version: file://{covdir}/index.html')
+    COV.erase()
 
 
 @app.shell_context_processor
